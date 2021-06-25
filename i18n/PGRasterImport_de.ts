417c8f772d49b82267cbb04b40c7d2175ed6f543
hdus <horst.duester@sourcepole.ch>
2021-06-25 11:25:38 +0200

    German translation added

---
+++ i18n/PGRasterImport_de.ts
@@ -0,0 +1,183 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../about/about.py" line="35"/>
+        <source>About {0} {1}</source>
+        <translation>Über {0} {1}</translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="36"/>
+        <source>Version: {0}</source>
+        <translation>Version: {0}</translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="38"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="39"/>
+        <source>Contact</source>
+        <translation>Kontakt</translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="40"/>
+        <source>Change Log</source>
+        <translation>Änderungen</translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="45"/>
+        <source>&lt;p&gt;&lt;center&gt;&lt;b&gt;Author(s):&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;center&gt;&lt;b&gt;Autor(en):&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="46"/>
+        <source>&lt;p&gt;{0}&lt;br&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="48"/>
+        <source>Sourcepole AG - Linux &amp; Open Source Solutions
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="49"/>
+        <source>Weberstrasse 5, 8004 Zurich, Switzerland
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="52"/>
+        <source>Contact:
+</source>
+        <translation>Kontakt:</translation>
+    </message>
+    <message>
+        <location filename="../about/about.py" line="55"/>
+        <source>Plugin Resources:
+</source>
+        <translation>Plugin-Ressourcen:</translation>
+    </message>
+</context>
+<context>
+    <name>PGRasterImport</name>
+    <message>
+        <location filename="../pgraster_import.py" line="177"/>
+        <source>&amp;PostGIS Raster Import</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import.py" line="164"/>
+        <source>PostGIS Raster Import</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>PGRasterImportDialog</name>
+    <message>
+        <location filename="../pgraster_import_dialog_base.py" line="128"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.py" line="128"/>
+        <source>No Database Connection Established.</source>
+        <translation>Es wurde keine Datenbankverbindung hergestellt.</translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.py" line="195"/>
+        <source>Table exists</source>
+        <translation>Die Tabelle existiert</translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.py" line="195"/>
+        <source>The selected table already exists in the database. Do you want to overwrite the table?</source>
+        <translation>Die ausgewählte Tabelle existiert bereits in der Datenbank. Möchten Sie die Tabelle überschreiben?</translation>
+    </message>
+</context>
+<context>
+    <name>PGRasterImportDialogBase</name>
+    <message>
+        <location filename="../pgraster_import_dialog_base.ui" line="14"/>
+        <source>PostGIS Raster Import</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.ui" line="30"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.ui" line="40"/>
+        <source>Tablename:</source>
+        <translation>Tabellenname:</translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.ui" line="50"/>
+        <source>Raster Layer:</source>
+        <translation>Rasterebene:</translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.ui" line="57"/>
+        <source>DB-Connection:</source>
+        <translation>DB-Verbindung:</translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.ui" line="64"/>
+        <source>Schema</source>
+        <translation>Schema:</translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.ui" line="100"/>
+        <source>Upload</source>
+        <translation>Hochladen</translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.ui" line="113"/>
+        <source>Close</source>
+        <translation>schliessen</translation>
+    </message>
+    <message>
+        <location filename="../pgraster_import_dialog_base.ui" line="133"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+</context>
+<context>
+    <name>RasterUpload</name>
+    <message>
+        <location filename="../raster/raster_upload.py" line="103"/>
+        <source>Creating table &apos;{table}&apos;...</source>
+        <translation>Tabelle &apos;{table}&apos; erstellen...</translation>
+    </message>
+    <message>
+        <location filename="../raster/raster_upload.py" line="125"/>
+        <source>Creating overview-level {level} for table &apos;{table}&apos;...</source>
+        <translation>Übersichtsebene {level} für Tabelle &apos;{table}&apos; anlegen...</translation>
+    </message>
+    <message>
+        <location filename="../raster/raster_upload.py" line="135"/>
+        <source>Registering raster columns of table &apos;%s&apos;...</source>
+        <translation>Rasterspalten der Tabelle &apos;%s&apos; werden registriert ...</translation>
+    </message>
+    <message>
+        <location filename="../raster/raster_upload.py" line="139"/>
+        <source>Upload successful finished</source>
+        <translation>Hochladen erfolgreich beendet</translation>
+    </message>
+    <message>
+        <location filename="../raster/raster_upload.py" line="644"/>
+        <source>Uploading tiles...</source>
+        <translation>Kacheln hochladen...</translation>
+    </message>
+    <message>
+        <location filename="../raster/raster_upload.py" line="690"/>
+        <source>Calculating raster params for {sum_tiles} tiles ...</source>
+        <translation>Berechnung von Rasterparametern für {sum_tiles}-Kacheln ...</translation>
+    </message>
+</context>
+</TS>
