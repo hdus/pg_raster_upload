d126e7f99163dbddd2c6a02ba820109cebe4708a
Horst <horst.duester@sourcepole.ch>
2021-06-25 16:55:15 +0200

    code cleaning

--- raster/raster_upload.py
+++ raster/raster_upload.py
@@ -22,14 +22,12 @@
 #
 from qgis.PyQt.QtCore import QObject, QFileInfo
 from qgis.PyQt.QtWidgets import QApplication,  QMessageBox
-from qgis.core import *
+#from qgis.core import *
 from osgeo import gdal
-from osgeo import osr
 import osgeo.gdalconst as gdalc
 from io import StringIO
 from qgiscloud.db_connections import DbConnections
 import binascii
-import glob
 import math
 import numpy
 import os
