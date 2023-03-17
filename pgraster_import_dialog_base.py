3688c956348852d9275ba520ef60e2cf89e04080
Florian Jenn <florian.jenn@gci-kw.de>
2023-03-17 12:03:21 +0100

    Don’t use SERVICE_NAME if empty

--- pgraster_import_dialog_base.py
+++ pgraster_import_dialog_base.py
@@ -122,8 +122,8 @@ class PGRasterImportDialog(QDialog, FORM_CLASS):
         DBPASSWD = str(settings.value(mySettings + '/password'))
         SERVICE_NAME = str(settings.value(mySettings + '/service'))
         
-        if SERVICE_NAME:
-            connection_info = "service='{0}'".format( SERVICE_NAME)
+        if SERVICE_NAME and SERVICE_NAME != 'NULL' and SERVICE_NAME != '':
+            connection_info = "service='{0}'".format(SERVICE_NAME)
         else:
             if DBPORT == None or DBPORT == 'NULL' or DBPORT == '':
                 DBPORT = '5432'
